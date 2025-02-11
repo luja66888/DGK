2c2d53026fe8e2eac07a5e9822f3417f5a127878
Dontosquare <153940558+Dontosquare@users.noreply.github.com>
2025-02-11 12:07:55 +0100

    Jeg har tilføjet verification til projektet

---
+++ Code/verification.js
@@ -0,0 +1,22 @@
+
+
+
+function checker() {
+    let age = document.getElementById('age').value.trim();
+    const message = document.getElementById('answer');
+
+    if (age === '') {
+        message.textContent = 'Indtast venligst din alder.';
+        message.style.color = 'white';
+        message.style.fontWeight = 'bold';
+
+    } else if (isNaN(age)) {
+        message.textContent = 'Indtast venligst et gyldigt tal.';
+        message.style.color = 'white';
+        message.styleW.fonteight = 'bold';
+    } else if (parseInt(age) < 18) {
+        window.location = href='https://www.google.com/';
+    } else {
+        window.location = href='https://dgkshop.com/';
+    }
+}
