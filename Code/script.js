45e13200b10aa881f6ffbd0049b56e23d23d8617
luja66888 <lucasjaegerpc@outlook.dk>
2025-02-13 10:19:54 +0100

    Tilføjet til index

--- Code/script.js
+++ Code/script.js
@@ -1,1 +1,17 @@
 // Hovedfilen
+
+// Slideshow
+let currentIndex = 0;
+function changeSlide(n) {
+    let slides = document.getElementsByClassName("slideshow__slide");
+    currentIndex += n;
+    if (currentIndex >= slides.length) currentIndex = 0;
+    if (currentIndex < 0) currentIndex = slides.length - 1;
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      slides[currentIndex].style.display = "block";
+    }
+    document.addEventListener("DOMContentLoaded", () => {
+        changeSlide(0);
+    });
